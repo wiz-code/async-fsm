c1d625ec4ccfe786cedec4bc468548121002f208
wiz-code <masa@wiz-code.digick.jp>
2017-08-24 17:08:52 +0900

    modified

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,13 @@ var buffer = require('vinyl-buffer');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('build', function () {
+gulp.task('dev-build', function () {
     browserify({
         entries: ['./src/async-fsm.js'],
         debug: true,
     }).
     bundle().
-    pipe(source('async-fsm.js')).
-    
-    pipe(gulp.dest('./dist')).
-    
-    pipe(
-        rename({
-            extname: '.min.js',
-        })
-    ).
-    
+    pipe(source('async-fsm.min.js')).
     pipe(buffer()).
     pipe(sourcemaps.init({loadMaps: true})).
     pipe(
@@ -32,6 +23,29 @@ gulp.task('build', function () {
         })
     ).
     pipe(sourcemaps.write()).
+    pipe(gulp.dest('./dev'));
+});
+
+gulp.task('build', function () {
+    browserify({
+        entries: ['./src/async-fsm.js'],
+    }).
+    bundle().
+    pipe(source('async-fsm.js')).
+    pipe(gulp.dest('./dist')).
+    pipe(
+        rename({
+            extname: '.min.js',
+        })
+    ).
+    pipe(buffer()).
+    pipe(
+        uglify({
+            mangle: {
+                keep_fnames: true,
+            },
+        })
+    ).
     pipe(gulp.dest('./dist'));
 });
 
